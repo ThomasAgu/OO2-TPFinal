f6c78dd095a65b4e4d8a0355440288120e548fea
Santiago Ponte <santiagoandresponteahon@hotmail.com>
2022-12-17 12:04:01 -0300

    Commit inicial

---
+++ src/main/java/com/demo/finaloo2/Finaloo2Application.java
@@ -0,0 +1,13 @@
+package com.demo.finaloo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finaloo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finaloo2Application.class, args);
+	}
+
+}
